c86868acff8346c0c67d7b54e3cb37d129994f97
stoney <chisomd90@gmail.com>
2025-08-11 15:44:01 +0100

    First Commit

---
+++ variable.js
@@ -0,0 +1,44 @@
+console.log("HELLO WORLD");
+console.log("CHISOM DANIEL");
+console.log(90)
+
+
+// Declare variables - x=3 y-2
+
+
+const age = 60;
+console.log(age);
+
+let y =90;
+y = 100;
+console.log(y);
+                    
+
+// rules of naming variables
+//1. Var cannot start with numbers, %
+//2. Reserved words
+//3. You can't have spaces in variables
+
+
+// Convention when naming variables
+//1. use logical and meaningful variables names 
+//2. Compound names e.g my Name acount Number my address
+//3.camelCasing
+
+const accountNumber = 7878883
+const myCountry = 'Togo'
+
+
+// detrmind if the var is going to be const or let
+//follow the rules and convention
+
+//your first name - const
+// your country of birth - const
+// The minimum eligible voting age in Nig -  let
+
+
+const firstName = 'Daniel';
+const country = "Nigeria";
+let minAge = "18";
+
+
